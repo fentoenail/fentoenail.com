54025341111dcf454faa03c29a36596e387afdaf
baquwerdz <dropthememes@gmail.com>
2025-10-26 14:29:20 +0100

    added some stuff

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const image = document.getElementById('mainImage');
+const hoverSound = document.getElementById("hoverSound");
+
+image.addEventListener("mouseEnter", () => {
+    hoverSound.currentTime = 0;
+    hoverSound.onplay();
+});
+
+image.addEventListener("mouseLeave", () => {
+    hoverSound.pause();
+    hoverSound.currentTime = 0;
+});
 
 image.addEventListener('click', () => {
     alert('Image clicked!');
