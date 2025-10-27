ec245ab7497286e831ae8daa947cf324a5cd26d7
baquwerdz <dropthememes@gmail.com>
2025-10-27 21:56:20 +0100

    did some stuff

--- VerNovember25/script.js
+++ VerNovember25/script.js
@@ -1,16 +1,14 @@
-const image = document.getElementById("mainImage");
+const image = document.getElementById("imageContainer");
 const hoverSound = document.getElementById("hoverSound");
 
 let audioUnlocked = false;
 
-// Unlock audio on first click
 document.addEventListener("click", () => {
   if (!audioUnlocked) {
     hoverSound.play().then(() => {
       hoverSound.pause();
       hoverSound.currentTime = 0;
       audioUnlocked = true;
-      console.log("Audio unlocked");
     }).catch(err => console.log("Audio unlock failed:", err));
   }
 });
@@ -32,3 +30,8 @@ image.addEventListener("mouseleave", () => {
 image.addEventListener("click", () => {
   alert("Image clicked!");
 });
+
+// Make an animation of a computer and on the screen, it shows the website. When user clicks on the computer, the computer zooms in to fill the screen and reveals the website fully.
+// Make the disc do something instead of just sending a warning.
+// 10 days before december 1st, make a count down appear on the screen.
+// When the user goes to the website on their phone, make an image with a computer and in the computer screen, there will be a link of the website. When user clicks on the computer, it prints "Visit this website on your computer"
